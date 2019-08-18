cfb7a2c000c05bbd10152a338dc6856c64ea1a9f
Rohitchaudhary99 <54234751+Rohitchaudhary99@users.noreply.github.com>
2019-08-18 03:17:57 -0700

    Add files via upload

---
+++ GRAVITATIONALFORCE.py
@@ -0,0 +1,7 @@
+gcons = input("enter G value")
+mass1 = input("enter M value")
+mass2 = input("enter m value")
+distt = input("enter distance between tow objects")
+result = float(gcons)*float(mass1)*float(mass2)/float(distt)*float(distt)
+
+print(result)
